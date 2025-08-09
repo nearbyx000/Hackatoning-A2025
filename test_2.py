aa94fb02e3f106565bd4a4c0ec2576f7069f624b
aleksandra-ivano-va <168302140+aleksandra-ivano-va@users.noreply.github.com>
2025-08-09 17:58:40 +0300

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 order_path = np.array([
-    1, 2, 3, 4, 5, 6, 7, 8,  9, 10,
-    11, 12, 1
-]) - 1
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 0
+]) 
 
 path = np.array([
     [0.0, 0.0], [0.0, 5.0], [0.0, 1.25], [2.0, 1.25],
@@ -20,13 +20,6 @@ client.set_height(0.7)
 
 for point_idx in order_path:
     y, x = path[point_idx]
-
-    # movement_completed = False
-
-    # def cb(msg):
-    #     global movement_completed
-    #     movement_completed = True
-    # # Старт движения
     client.go_to_xy_nav(x, y)
 
 print(client.landing())
