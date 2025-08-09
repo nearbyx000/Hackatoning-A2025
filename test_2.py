3c1aac6853ecdd6e16bbfd1134ff7deb798e7749
nearbyx000 <misterpipka881@gmail.com>
2025-08-09 23:37:01 +0300

    leassure drone controller

--- test_2.py
+++ test_2.py
@@ -3,9 +3,9 @@ import numpy as np
 import time
 
 order_path = np.array([
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    10, 11, 0
-]) 
+    1, 2, 3, 4, 5, 6, 7, 8,  9, 10,
+    11, 12, 1
+]) - 1
 
 path = np.array([
     [0.0, 0.0], [0.0, 5.0], [0.0, 1.25], [2.0, 1.25],
@@ -20,10 +20,16 @@ client.set_height(0.5)
 
 for point_idx in order_path:
     y, x = path[point_idx]
+
+    # movement_completed = False
+
+    # def cb(msg):
+    #     global movement_completed
+    #     movement_completed = True
+    # # Старт движения
     client.go_to_xy_nav(x, y)
     
 print(client.get_nav_pose())
 
 print(client.landing())
 print(client.disconnect())
-23
