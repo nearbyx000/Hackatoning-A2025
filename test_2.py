e0342e65205849a624454d5743dfc3a303791889
nearbyx000 <misterpipka881@gmail.com>
2025-08-09 23:21:42 +0300

    small drone controll

--- test_2.py
+++ test_2.py
@@ -1,6 +1,6 @@
 from drone_control_api import Drone
 import numpy as np
-
+import time
 
 order_path = np.array([
     1, 2, 3, 4, 5, 6, 7, 8,  9, 10,
@@ -12,11 +12,11 @@ path = np.array([
     [2.0, 0.8], [3.0, 0.8], [2.5, 3.5], [4.0, 3.5],
     [5.0, 3.5], [5.0, 1.0], [5.0, 1.75], [0.0, 1.75]
 ]) 
-
+ 
 client = Drone()
 print(client.connect("192.168.41.21", reset_state=True))
 print(client.takeoff())
-client.set_height(0.7)
+client.set_height(0.5)
 
 for point_idx in order_path:
     y, x = path[point_idx]
@@ -28,6 +28,9 @@ for point_idx in order_path:
     #     movement_completed = True
     # # Старт движения
     client.go_to_xy_nav(x, y)
+    
+print(client.get_nav_pose())
 
 print(client.landing())
 print(client.disconnect())
+23
