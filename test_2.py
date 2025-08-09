3ef2be14399fc5b419c5d43ed8d6c893ec25cb61
aleksandra-ivano-va <168302140+aleksandra-ivano-va@users.noreply.github.com>
2025-08-09 16:21:03 +0300

    Add files via upload

---
+++ test_2.py
@@ -0,0 +1,34 @@
+from drone_control_api import Drone
+import numpy as np
+import time
+import math
+import json
+
+order_path = np.array([
+    1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10,
+    11, 12, 1
+]) - 1
+path = np.array([
+    [0.0, 0.0], [0.0, 6.0], [0.0, 1.25], [2.0, 1.25],
+    [2.0, 0.5], [0.5, 3.0], [2.5, 3.5], [4.0, 3.5],
+    [6.0, 3.5], [6.0, 0.0], [6.0, 1.75], [0.0, 1.75]
+]) 
+
+client = Drone()
+print(client.connect("10.42.0.1", reset_state=True))
+print(client.takeoff())
+time.sleep(5.0)
+
+for i, point_idx in enumerate(order_path):
+    x, y = path[point_idx]
+    movement_completed = False
+
+    def cb(msg):
+        global movement_completed
+        movement_completed = True
+    # Старт движения
+    client.go_to_xy_nav_nb(x, y, callback=cb)
+
+print(client.landing())
+print(client.disconnect())
+
