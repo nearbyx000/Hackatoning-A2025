24b72fb9932f9f5aee823618f53d25a09a7d28ec
aleksandra-ivano-va <168302140+aleksandra-ivano-va@users.noreply.github.com>
2025-08-09 17:06:52 +0300

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,23 +1,22 @@
 from drone_control_api import Drone
 import numpy as np
-import time
-import math
-import json
+
 
 order_path = np.array([
-    1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10,
+    1, 2, 3, 4, 5, 6, 7, 8,  9, 10,
     11, 12, 1
 ]) - 1
+
 path = np.array([
-    [0.0, 0.0], [0.0, 6.0], [0.0, 1.25], [2.0, 1.25],
-    [2.0, 0.5], [0.5, 3.0], [2.5, 3.5], [4.0, 3.5],
-    [6.0, 3.5], [6.0, 0.0], [6.0, 1.75], [0.0, 1.75]
+    [0.0, 0.0], [0.0, 5.0], [0.0, 1.25], [2.0, 1.25],
+    [2.0, 0.8], [3.0, 0.8], [2.5, 3.5], [4.0, 3.5],
+    [5.0, 3.5], [5.0, 1.0], [5.0, 1.75], [0.0, 1.75]
 ]) 
 
 client = Drone()
 print(client.connect("192.168.41.21", reset_state=True))
 print(client.takeoff())
-client.set_height(0.5)
+client.set_height(0.7)
 
 for point_idx in order_path:
     y, x = path[point_idx]
@@ -32,5 +31,3 @@ for point_idx in order_path:
 
 print(client.landing())
 print(client.disconnect())
-
-
