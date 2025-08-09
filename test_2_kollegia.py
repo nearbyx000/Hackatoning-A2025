600e07554a2ff5f0ecb5f9ac5847d123b6d81190
aleksandra-ivano-va <168302140+aleksandra-ivano-va@users.noreply.github.com>
2025-08-09 23:16:56 +0300

    Add files via upload

---
+++ test_2_kollegia.py
@@ -0,0 +1,25 @@
+from drone_control_api import Drone
+import numpy as np
+
+order_path = np.array([
+    0, 1, 2, 3, 4, 5, 6, 7,  8, 9,
+    10, 11, 12, 13, 0
+]) 
+
+path = np.array([
+    [0.0, 0.0], [0.0, 5.0], [0.0, 1.25], [2.0, 1.25],
+    [2.0, 0.8], [3.0, 0.8], [2.5, 3.5], [4.0, 3.5],
+    [5.0, 3.5], [5.0, 1.0], [5.0, 3.0], [2.5, 3.0], [2.5, 1.75], [0.0, 1.75], 
+]) 
+
+client = Drone()
+print(client.connect("192.168.41.21", reset_state=True))
+print(client.takeoff())
+client.set_height(0.5)
+
+for point_idx in order_path:
+    y, x = path[point_idx]
+    client.go_to_xy_nav(x, y)
+
+print(client.landing())
+print(client.disconnect())
