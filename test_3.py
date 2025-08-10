d77a52348c16e49e65827dc205c6484b77314c7f
aleksandra-ivano-va <alex03ivanova@yandex.ru>
2025-08-10 12:34:44 +0300

    Update test_3.py

--- test_3.py
+++ test_3.py
@@ -5,9 +5,9 @@ import math
 import json
 
 order_path = np.array([
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9,
+    0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9,
     10, 11, 12, 13, 0
-]) - 1
+]) 
 
 order_yaw = np.array([
     1, 4, 3, 4, 3, 5, 3, 1, 3, 4, 1,
@@ -23,33 +23,38 @@ path = np.array([
 
 client = Drone()
 print(client.connect("192.168.41.21", reset_state=True))
+high = False
 print(client.takeoff())
+client.set_height(0.5)
 time.sleep(5.0)
+high = True
+if high == True:
+    for i, point_idx in enumerate(order_path):
+        x, y = path[point_idx]
+        movement_completed = False
 
-for i, point_idx in enumerate(order_path):
-    x, y = path[point_idx]
-    movement_completed = False
+        def cb(msg):
+            global movement_completed
+            movement_completed = True
+        # Старт движения
 
-    def cb(msg):
-        global movement_completed
-        movement_completed = True
-    # Старт движения
-    
-    client.go_to_xy_nav_nb(x, y, callback=cb)
-    
-    if i < len(order_yaw):
-        yaw_idx = order_yaw[i]
-        yaw_angle = yaw_angles[yaw_idx]
+        client.go_to_xy_nav_nb(x, y, callback=cb)
+        
+        if i < len(order_yaw):
+            yaw_idx = order_yaw[i]
+            yaw_angle = yaw_angles[yaw_idx]
 
-        rotation_completed = False
-        def rotation_callback(msg):
-            global rotation_completed
-            rotation_completed = True
+            rotation_completed = False
+            def rotation_callback(msg):
+                global rotation_completed
+                rotation_completed = True
 
-        client.set_yaw_nb(yaw_angle, callback=rotation_callback)
-        while not rotation_completed:
-            time.sleep(0.05)
-
+            client.set_yaw_nb(yaw_angle, callback=rotation_callback)
+            while not rotation_completed:
+                time.sleep(0.05)
+ы
 print(client.landing())
 print(client.disconnect())
+
+#111A777&Sa$
 
